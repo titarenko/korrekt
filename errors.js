32c94350e34f43de2133a75b02e20a438d86cb6b
Constantin Titarenko <constantin.titarenko@gmail.com>
2016-08-12 11:28:34 +0300

    add message and call stack to validation error

--- errors.js
+++ errors.js
@@ -3,8 +3,10 @@ const util = require('util')
 module.exports = { ValidationError }
 
 function ValidationError (results) {
+	this.message = 'validation failed'
 	this.fields = { }
 	results.forEach(result => this.fields[result.field] = result.value)
+	Error.captureStackTrace(this, ValidationError)
 }
 
 util.inherits(ValidationError, Error)
